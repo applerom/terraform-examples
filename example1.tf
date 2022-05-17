6bba3ee900c6a1d644102267a12bebe827985446
Roman Sevko <apple@rom.by>
2022-05-17 18:20:58 +0300

    vpc1

--- example1.tf
+++ example1.tf
@@ -1,8 +1,40 @@
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.9.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main3245232342134"
+    Name = "main"
   }
+}
+
+resource "aws_subnet" "subnet-public-1" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.9.1.0/24"
+    map_public_ip_on_launch = "true" //it makes this a public subnet
+    availability_zone = "eu-central-1"
+    tags = {
+        Name = "subnet-public-1"
+    }
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = "${aws_vpc.main.id}"
+    tags = {
+        Name = "prod-igw"
+    }
+}
+
+resource "aws_route_table" "public-rt" {
+    vpc_id = "${aws_vpc.main.id}"
+    
+    route {
+        //associated subnet can reach everywhere
+        cidr_block = "0.0.0.0/0" 
+        //CRT uses this IGW to reach internet
+        gateway_id = "${aws_internet_gateway.igw.id}" 
+    }
+    
+    tags = {
+        Name = "public-rt"
+    }
 }
