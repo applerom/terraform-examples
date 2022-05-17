77646cd0d72f5eb1bae93f30bc245a301d1e979f
Roman Sevko <apple@rom.by>
2022-05-17 19:11:10 +0300

    Update example1.tf

--- example1.tf
+++ example1.tf
@@ -43,3 +43,39 @@ resource "aws_route_table_association" "rta-public-subnet-1"{
     subnet_id = "${aws_subnet.subnet-public-1.id}"
     route_table_id = "${aws_route_table.public-rt.id}"
 }
+
+resource "aws_security_group" "webserver" {
+    vpc_id = "${aws_vpc.vpc.id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        // This means, all ip address are allowed to ssh ! 
+        // Do not do it in the production. 
+        // Put your office or home address in it!
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    //If you do not add this rule, you can not reach the NGIX  
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "webserver"
+    }
+}
