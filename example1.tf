931aa10f0bdbef6d3a050c06260d0b855ecd1c96
Roman Sevko <apple@rom.by>
2022-05-19 16:45:08 +0300

    fixed ipv6 cidr blocks

--- example1.tf
+++ example1.tf
@@ -72,7 +72,13 @@ resource "aws_security_group" "webserver" {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = ["0.0.0.0/0","::/0"]
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        ipv6_cidr_blocks = ["::/0"]
     }
     ingress {
         from_port = 22
@@ -81,7 +87,17 @@ resource "aws_security_group" "webserver" {
         // This means, all ip address are allowed to ssh ! 
         // Do not do it in the production. 
         // Put your office or home address in it!
-        cidr_blocks = ["0.0.0.0/0","::/0"]
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "SSH port"
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        // This means, all ip address are allowed to ssh ! 
+        // Do not do it in the production. 
+        // Put your office or home address in it!
+        ipv6_cidr_blocks = ["::/0"]
         description = "SSH port"
     }
     //If you do not add this rule, you can not reach the NGIX  
@@ -89,14 +105,28 @@ resource "aws_security_group" "webserver" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0","::/0"]
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTP port"
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        ipv6_cidr_blocks = ["::/0"]
         description = "HTTP port"
     }
     ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0","::/0"]
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTPS port"
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        ipv6_cidr_blocks = ["::/0"]
         description = "HTTPS port"
     }
     tags = {
