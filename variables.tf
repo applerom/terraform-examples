3a52015917a824fa479bc127492fa7e5265607c7
applerom <apple@rom.by>
2022-07-10 20:41:38 +0300

    some changes

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
-variable "AMI" {
+variable "AMI-Ubuntu" {
+    type = map
+    default = {
+        eu-central-1 = "ami-07cd3675e7ca644fe"
+        eu-north-1 = "ami-0adfc82c1ad9f327b"
+    }
+}
+
+variable "AMI-AL2" {
     type = map
     default = {
         eu-central-1 = "ami-07cd3675e7ca644fe"
