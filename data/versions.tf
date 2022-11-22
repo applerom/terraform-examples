469bf6c6778e821d08abbe59e61f7d3858b31b61
Roman Sevko <apple@rom.by>
2022-11-22 14:27:58 +0300

    added cloudshell

--- data/versions.tf
+++ data/versions.tf
@@ -8,7 +8,7 @@ terraform {
   #backend "s3" {
   #  bucket         = "tf-state-my"
   #  dynamodb_table = "tf-state-my-lock"
-  #  key            = "tf-backend.tfstate"
+  #  key            = "data.tfstate"
   #  region         = "us-east-1"
   #  encrypt        = "true"
   #}
